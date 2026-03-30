8ecd23c2d555b0dc9be464c9cb5410613afde352
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2026-03-30 11:27:22 -0300

    feat(config): add k6 options with thresholds and scenario config

---
+++ config/options.js
@@ -0,0 +1,32 @@
+import { SharedArray } from 'k6/data';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+
+const BASE_URL = 'https://fakestoreapi.com';
+
+const TARGET_TPS = 20;
+const MAX_RESPONSE_TIME_MS = 1500;
+const MAX_ERROR_RATE_PERCENT = 3;
+
+export const options = {
+  scenarios: {
+    login_load_test: {
+      executor: 'constant-arrival-rate',
+      rate: TARGET_TPS,
+      timeUnit: '1s',
+      duration: '1m',
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+    },
+  },
+  thresholds: {
+    http_req_duration: [`p(95)<${MAX_RESPONSE_TIME_MS}`],
+    http_req_failed: [`rate<${MAX_ERROR_RATE_PERCENT / 100}`],
+  },
+};
+
+export const users = new SharedArray('users', function () {
+  const file = open('../data/users.csv');
+  return papaparse.parse(file, { header: true }).data;
+});
+
+export { BASE_URL, TARGET_TPS, MAX_RESPONSE_TIME_MS, MAX_ERROR_RATE_PERCENT };
